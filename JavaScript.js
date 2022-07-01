5d1f46e8e60d4258bd1beffbac79537d8fe24340
KhaiNguyen1608 <nguyenkhai.huflit@gmail.com>
2022-07-01 15:59:16 +0700

    init project

---
+++ JavaScript.js
@@ -0,0 +1,38 @@
+
+
+
+//Bài tập 1:
+var workDays =  24;
+var result1 = workDays * 100000;
+console.log("Tổng lương trong tháng là:", result1);
+
+//Bài tập 2
+var numb1 = 232;
+var numb2 = 34;
+var numb3 = 87;
+var numb4 = 53;
+var numb5 = 12;
+var result2 = (numb1 + numb2 + numb3 + numb4 + numb5) / 5;
+console.log("Giá trị trung bình:",result2);
+
+//Bài tập 3
+var usd = 4;
+var vnd = 23.500;
+var result3 = usd * vnd;
+console.log("Số tiền sau khi quy đổi:",result3);
+
+//Bài tập 4
+var recWidth = 10;
+var recHeight = 4.5;
+var result41 = recHeight * recWidth;
+var result42 = (recHeight + recWidth) * 2;
+console.log("Diện tích:",result41);
+console.log("Chu vi:",result42);
+
+
+//Bài tập 5
+var tmpNumber = 84;
+var so_hang_dv = 84 % 10;
+var so_hang_chuc = Math.round(84/10);
+var result5 = so_hang_dv + so_hang_chuc;
+console.log("Tổng 2 ký số của số vừa nhập:",result5);
