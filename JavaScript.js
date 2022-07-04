2283544dc4a8e0a90afa2e5c073691ecf7e049e0
KhaiNguyen1608 <nguyenkhai.huflit@gmail.com>
2022-07-04 16:26:07 +0700

    update sơ đồ 3 khối

--- JavaScript.js
+++ JavaScript.js
@@ -2,37 +2,52 @@
 
 
 //Bài tập 1:
+//Input
 var workDays =  24;
+//Process
 var result1 = workDays * 100000;
+//Output
 console.log("Tổng lương trong tháng là:", result1);
 
 //Bài tập 2
+//Input
 var numb1 = 232;
 var numb2 = 34;
 var numb3 = 87;
 var numb4 = 53;
 var numb5 = 12;
+//Process
 var result2 = (numb1 + numb2 + numb3 + numb4 + numb5) / 5;
+//Output
 console.log("Giá trị trung bình:",result2);
 
 //Bài tập 3
+//Input
 var usd = 4;
 var vnd = 23.500;
+//Process
 var result3 = usd * vnd;
+//Output
 console.log("Số tiền sau khi quy đổi:",result3);
 
 //Bài tập 4
+//Input
 var recWidth = 10;
 var recHeight = 4.5;
+//Process
 var result41 = recHeight * recWidth;
 var result42 = (recHeight + recWidth) * 2;
+//Output
 console.log("Diện tích:",result41);
 console.log("Chu vi:",result42);
 
 
 //Bài tập 5
+//Input
 var tmpNumber = 84;
-var so_hang_dv = 84 % 10;
-var so_hang_chuc = Math.round(84/10);
+//Process
+var so_hang_dv = tmpNumber % 10;
+var so_hang_chuc = Math.round(tmpNumber/10);
 var result5 = so_hang_dv + so_hang_chuc;
+//Output
 console.log("Tổng 2 ký số của số vừa nhập:",result5);
